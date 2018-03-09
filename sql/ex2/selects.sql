54952ab42057122a221731d105c863796cd8ee04
Carlos Roberto Ferreira Sanches <carlos-caps@hotmail.com>
2018-03-09 09:44:26 -0300

    SELECIONADO E ARRUMADO

--- sql/ex2/selects.sql
+++ sql/ex2/selects.sql
@@ -1,28 +1,23 @@
 -- 1
-
-select count(distinct p.id) as logins, MONTH(p.login_date) as login_month
+select	distinct MONTH(p.login_date)  as login_month, count(distinct p.id_user) as logins
 from dbo.logins as p
 group by MONTH(p.login_date)
-order by logins;
+order by login_month;
 
 -- 2
-
-select avg(p.id) as media, DAY(p.login_) as day
-from (select TOP(30) logins.login_date as login_, dbo.logins.id as id
-		from dbo.logins
-		order by logins.login_date desc) as p
-group by p.login_
-order by media desc;
+select avg(p.id) as media
+from (	select distinct TOP(30) DAY(logins.login_date) as login_, count(dbo.logins.id) as id
+		from dbo.logins	
+		group by DAY(logins.login_date)
+		order by login_ desc) as p;
 
 -- 3
-
-select sum(d.length)
+select sum(d.length) as 'espaço total'
 from (select top(10) dbo.stored_files.length as length
 		from dbo.stored_files
 		order by length desc) as d;
 
 -- 4
-
 select s.name
 from ( select count(s.id) as cnt, s.name as name 
 	   from dbo.states as s join dbo.cities as c 
